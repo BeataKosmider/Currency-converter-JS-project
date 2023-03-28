58079090e76155a8981c95fad239fe74cca74f31
Beata Kosmider <kosmibe@gmail.com>
2023-03-28 23:11:26 +0200

    html, css and js files added

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("exchange-form");
+const amountPLN = document.getElementById("PLN");
+const currency = document.getElementById("currency");
+const input = document.getElementById("amount");
+const loader = document.getElementById("loader");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  loader.style.display = "block";
+
+  fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currency.value}/today/`)
+    .then((response) => response.json())
+    .then((data) => {
+      const rate = data.rates[0].mid;
+      const result = input.value * rate;
+      amountPLN.innerHTML = result.toFixed(2);
+    })
+    .catch((error) => {
+      console.error(error);
+      amountPLN.innerHTML = "No actual data available for today";
+    })
+    .finally(() => {
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 1.5 * 1000);
+    });
+});
